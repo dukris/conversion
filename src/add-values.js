f039dc8780c571c5651748faca6786fbdc8c3dd6
hizmailovich <theannalios@gmail.com>
2023-07-20 16:35:58 +0300

    Function AddValues implemented

---
+++ src/add-values.js
@@ -0,0 +1,20 @@
+/**
+ * Add values.
+ *
+ * @param a First value
+ * @param b Second value
+ * @returns {*} Result
+ */
+export function addValues(a, b) {
+    if (a === undefined || b === undefined || a === null || b === null
+        || ((typeof a === "object" && !Array.isArray(a)) || (typeof b === "object" && !Array.isArray(b)))) {
+        throw new TypeError("The addition is not possible!");
+    }
+    if (typeof a === "symbol") {
+        a = a.description;
+    }
+    if (typeof b === "symbol") {
+        b = b.description;
+    }
+    return a + b;
+}
